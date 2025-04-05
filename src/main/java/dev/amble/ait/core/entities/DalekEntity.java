d8c709b9c0e19928cd91c32a2a20e2c932080c56
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-04-06 00:29:59 +0200

    chore: made daleks attack hostile mobs aswell

--- src/main/java/dev/amble/ait/core/entities/DalekEntity.java
+++ src/main/java/dev/amble/ait/core/entities/DalekEntity.java
@@ -101,6 +101,7 @@ public class DalekEntity extends RaiderEntity implements RangedAttackMob {
         this.goalSelector.add(6, new LookAroundGoal(this));
         this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
         this.targetSelector.add(1, new ActiveTargetGoal<>(this, PassiveEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, HostileEntity.class, true));
         this.targetSelector.add(2, new RevengeGoal(this));
     }
 
