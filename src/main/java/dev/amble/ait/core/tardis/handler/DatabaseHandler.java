9e972f093eccf22a46fd0bbaa9d2d51f8d753c9e
Loqor <isaiahcrace@gmail.com>
2025-06-22 15:18:47 -0500

    fix: add uuid set and switched from using strings to uuids because strings is silly

--- src/main/java/dev/amble/ait/core/tardis/handler/DatabaseHandler.java
+++ src/main/java/dev/amble/ait/core/tardis/handler/DatabaseHandler.java
@@ -1,15 +1,16 @@
 package dev.amble.ait.core.tardis.handler;
 
 import java.util.HashSet;
+import java.util.UUID;
 
 import dev.amble.ait.api.tardis.KeyedTardisComponent;
 import dev.amble.ait.data.properties.Property;
 import dev.amble.ait.data.properties.Value;
 
 public class DatabaseHandler extends KeyedTardisComponent {
-    private static final Property<HashSet<String>> PEOPLE = new Property<>(Property.STR_SET, "people",
+    private static final Property<HashSet<UUID>> PEOPLE = new Property<>(Property.UUID_SET, "people",
             new HashSet<>());
-    private final Value<HashSet<String>> people = PEOPLE.create(this);
+    private final Value<HashSet<UUID>> people = PEOPLE.create(this);
     public DatabaseHandler() {
         super(Id.DATABASE);
     }
@@ -20,15 +21,15 @@ public class DatabaseHandler extends KeyedTardisComponent {
         people.of(this, PEOPLE);
     }
 
-    public void addPerson(String person) {
+    public void addPerson(UUID person) {
         addPerson(person, true);
     }
 
-    public void addPerson(String person, boolean sync) {
-        if (person == null || person.isEmpty()) return;
-        people.flatMap(strings -> {
-            strings.add(person);
-            return strings;
+    public void addPerson(UUID person, boolean sync) {
+        if (person == null) return;
+        people.flatMap(uuids -> {
+            uuids.add(person);
+            return uuids;
         }, sync);
     }
 }
