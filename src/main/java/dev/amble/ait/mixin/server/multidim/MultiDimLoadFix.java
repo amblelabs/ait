71fffeea21a3fb9137bec05f350b03284d3ed674
Theo <git@theo.is-a.dev>
2025-09-12 10:02:14 +0300

    fix respawn?
    Signed-off-by: Theo <git@theo.is-a.dev>

--- src/main/java/dev/amble/ait/mixin/server/multidim/MultiDimLoadFix.java
+++ src/main/java/dev/amble/ait/mixin/server/multidim/MultiDimLoadFix.java
@@ -4,36 +4,34 @@ import java.util.UUID;
 
 import com.mojang.datafixers.util.Either;
 import dev.amble.lib.data.CachedDirectedGlobalPos;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 
-import dev.amble.ait.AITMod;
 import dev.amble.ait.core.tardis.ServerTardis;
 import dev.amble.ait.core.tardis.handler.travel.TravelHandler;
 import dev.amble.ait.core.tardis.manager.ServerTardisManager;
 import dev.amble.ait.core.world.TardisServerWorld;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerManager.class)
+@Mixin(MinecraftServer.class)
 public class MultiDimLoadFix {
 
-    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getWorld(Lnet/minecraft/registry/RegistryKey;)Lnet/minecraft/server/world/ServerWorld;"))
-    public @Nullable ServerWorld getWorld(MinecraftServer instance, RegistryKey<World> key) {
-        ServerWorld result = instance.getWorld(key);
+    @Inject(method = "getWorld", at = @At("RETURN"), cancellable = true)
+    public void getWorld(RegistryKey<World> key, CallbackInfoReturnable<ServerWorld> cir) {
+        if (cir.getReturnValue() != null || TardisServerWorld.isTardisDimension(key))
+            return;
 
-        if (result != null)
-            return result;
-
-        return ait$loadTardisFromWorld(instance, key);
+        cir.setReturnValue(ait$loadTardisFromWorld((MinecraftServer) (Object) this, key));
     }
 
+    @Unique
     public ServerWorld ait$loadTardisFromWorld(MinecraftServer server, RegistryKey<World> key) {
         if (!TardisServerWorld.isTardisDimension(key))
             return null;
@@ -45,11 +43,6 @@ public class MultiDimLoadFix {
 
         if (either == null)
             either = manager.loadTardis(server, id);
-
-        if (either == null) {
-            AITMod.LOGGER.error("Failed to load world for {}", id);
-            return null;
-        }
 
         ServerTardis tardis = either.map(t -> t, o -> null);
 
