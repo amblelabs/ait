e39c05887a2465d5498884ddd34b46be035e04b3
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-09-12 03:16:36 +0100

    qw
    qweqwe

--- src/main/java/dev/amble/ait/client/sounds/engine/EngineLoopSound.java
+++ src/main/java/dev/amble/ait/client/sounds/engine/EngineLoopSound.java
@@ -1,18 +1,23 @@
 package dev.amble.ait.client.sounds.engine;
 
+import dev.amble.ait.client.AITModClient;
 import dev.amble.ait.core.blockentities.EngineBlockEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 
 import dev.amble.ait.client.sounds.PositionedLoopingSound;
 import dev.amble.ait.client.util.ClientTardisUtil;
 import dev.amble.ait.core.AITSounds;
+import net.minecraft.world.World;
+
+import java.util.UUID;
 
 public class EngineLoopSound extends PositionedLoopingSound {
     private int ticks = 0;
 
     public EngineLoopSound() {
-        super(AITSounds.ENGINE_LOOP, SoundCategory.BLOCKS, new BlockPos(0, 0, 0), 0.35f);
+        super(AITSounds.ENGINE_LOOP, SoundCategory.BLOCKS, new BlockPos(0, 0, 0), AITModClient.CONFIG.engineLoopVolume);
     }
 
     @Override
@@ -20,18 +25,29 @@ public class EngineLoopSound extends PositionedLoopingSound {
         super.tick();
         this.ticks++;
 
+        this.setVolume(EngineBlockEntity.doesEngineBlockExist(getTardisUuid()) ? AITModClient.CONFIG.engineLoopVolume : 0);
+
         if (this.ticks >= (40)) {
             this.refresh();
         }
     }
 
     public void refresh() {
-        if (EngineBlockEntity.doesEngineThere()) {
+        if (EngineBlockEntity.doesEngineBlockExist(getTardisUuid())) {
             this.setPosition(ClientTardisUtil.getNearestEngine());
         } else {
-            BlockPos g = new BlockPos((29999984 - 30000 - (int) Math.floor(3000 + Math.random() * (2 + 3))), 200 - 95 + 5, 29999984 - 367);
-            this.setPosition(g);
+            this.setVolume(0);
+            
         }
         this.ticks = 0;
+    }
+
+    public UUID getTardisUuid() {
+        BlockPos pos = ClientTardisUtil.getNearestEngine();
+        World world = MinecraftClient.getInstance().world;
+        if (world != null && world.getBlockEntity(pos) instanceof EngineBlockEntity engine) {
+            return engine.tardis().getId();
+        }
+        return null;
     }
 }
