a9b2139b7a8ae1721fcb316df9813d73a5f0dc78
Pan <847687+Mansarde@users.noreply.github.com>
2025-10-03 17:45:19 +0200

    move eatFood method into existing LivingEntity mixin

--- src/main/java/dev/amble/ait/mixin/LivingEntityMixin.java
+++ src/main/java/dev/amble/ait/mixin/LivingEntityMixin.java
@@ -1,5 +1,8 @@
 package dev.amble.ait.mixin;
 
+import java.util.UUID;
+
+import com.mojang.datafixers.util.Either;
 import net.fabricmc.fabric.api.util.TriState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,10 +20,17 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
 
 import dev.amble.ait.api.ExtraPushableEntity;
 import dev.amble.ait.core.AITTags;
+import dev.amble.ait.core.tardis.ServerTardis;
+import dev.amble.ait.core.tardis.handler.mood.v2.Emotion;
+import dev.amble.ait.core.tardis.handler.mood.v2.MoodHandler2;
+import dev.amble.ait.core.tardis.manager.ServerTardisManager;
 import dev.amble.ait.core.world.TardisServerWorld;
 
 @Mixin(LivingEntity.class)
@@ -64,6 +74,36 @@ public abstract class LivingEntityMixin extends Entity implements ExtraPushableE
     @Override
     public TriState ait$pushBehaviour() {
         return ait$pushable;
+    }
+
+    @Inject(
+            method = "eatFood",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;decrement(I)V",
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void ait$eatFood(World world, ItemStack stack, CallbackInfoReturnable<ItemStack> cir){
+        NbtCompound cookietag = stack.getNbt();
+        boolean hasTagWithData = cookietag != null && cookietag.contains("Identifier") && cookietag.contains("Data", 10);
+
+        if (stack.getItem() == Items.COOKIE && hasTagWithData){
+            NbtCompound data = cookietag.getCompound("Data");
+            UUID uuid = data.getUuid("Uuid");
+
+            MinecraftServer server = world.getServer();
+            if (server != null) {
+                Either<ServerTardis, ?> either = ServerTardisManager.getInstance().lookup().get(uuid);
+                if (either != null) {
+                    ServerTardis tardis = either.map(t -> t, o -> null);
+                    if (tardis != null) {
+                        MoodHandler2 mood = new MoodHandler2();
+                        mood.add(Emotion.Type.CONTENT, data.getFloat("Multiplier"));
+                    }
+                }
+            }
+        }
     }
 
     @Inject(method = "isPushable", at = @At("RETURN"), cancellable = true)
