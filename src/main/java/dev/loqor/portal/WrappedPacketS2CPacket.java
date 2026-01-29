836be6ddc9d6e2696e320d35dd5d492af293e26c
Theo <git@theo.is-a.dev>
2026-01-29 19:27:01 +0300

    fix: bundle packets no longer explode

--- src/main/java/dev/loqor/portal/WrappedPacketS2CPacket.java
+++ src/main/java/dev/loqor/portal/WrappedPacketS2CPacket.java
@@ -6,25 +6,69 @@ import net.fabricmc.fabric.api.networking.v1.PacketType;
 import net.minecraft.network.NetworkSide;
 import net.minecraft.network.NetworkState;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.BundleS2CPacket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public record WrappedPacketS2CPacket(Packet<?> packet) implements FabricPacket, ProxiedPacket {
 
     public static final PacketType<WrappedPacketS2CPacket> TYPE = PacketType.create(AITMod.id("wrapped"), WrappedPacketS2CPacket::read);
 
     private static WrappedPacketS2CPacket read(PacketByteBuf buf) {
-        int packetId = buf.readVarInt();
-        Packet<?> packet = NetworkState.PLAY.getPacketHandler(NetworkSide.CLIENTBOUND, packetId, buf);
-
+        Packet<?> packet = readPacket(buf);
         return new WrappedPacketS2CPacket(packet);
     }
 
     @Override
     public void write(PacketByteBuf buf) {
-        int packetId = NetworkState.PLAY.getPacketId(NetworkSide.CLIENTBOUND, this.packet);
+        if (this.packet instanceof BundleS2CPacket bundle) {
+            List<Packet<ClientPlayPacketListener>> packets = (List<Packet<ClientPlayPacketListener>>) bundle.getPackets();
+            buf.writeVarInt(packets.size());
 
-        buf.writeVarInt(packetId);
-        this.packet.write(buf);
+            for (Packet<ClientPlayPacketListener> packet : packets) {
+                writePacket(packet, buf);
+            }
+        } else {
+            writePacket(this.packet, buf);
+        }
+    }
+
+    private static int getPacketId(Packet<?> packet) {
+        if (packet instanceof BundleS2CPacket)
+            return -1;
+
+        int packetId = NetworkState.PLAY.getPacketId(NetworkSide.CLIENTBOUND, packet);
+
+        if (packetId < 0)
+            throw new IllegalStateException("Bad packet id: " + packetId + " for packet " + packet);
+
+        return packetId;
+    }
+
+    private static void writePacket(Packet<?> packet, PacketByteBuf buf) {
+        buf.writeVarInt(getPacketId(packet));
+        packet.write(buf);
+    }
+
+    private static Packet<ClientPlayPacketListener> readPacket(PacketByteBuf buf) {
+        int packetId = buf.readVarInt();
+
+        if (packetId == -1) {
+            int size = buf.readVarInt();
+            List<Packet<ClientPlayPacketListener>> packets = new ArrayList<>(size);
+
+            for (int i = 0; i < size; i++) {
+                packets.add(readPacket(buf));
+            }
+
+            return new BundleS2CPacket(packets);
+        } else {
+            //noinspection unchecked - source: trust me bro.
+            return (Packet<ClientPlayPacketListener>) NetworkState.PLAY.getPacketHandler(NetworkSide.CLIENTBOUND, packetId, buf);
+        }
     }
 
     @Override
