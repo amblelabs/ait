a0340123ec561300609d5cc31d88308e01182fd9
Addi3 <addiec787@gmail.com>
2026-06-08 21:46:47 +0100

    chore: use Properties.FACING

--- src/main/java/dev/amble/ait/core/blocks/TardisCoralFanBlock.java
+++ src/main/java/dev/amble/ait/core/blocks/TardisCoralFanBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -16,7 +17,7 @@ import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 
 public class TardisCoralFanBlock extends Block implements Waterloggable {
-    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final DirectionProperty FACING = Properties.FACING;
     public static final BooleanProperty WATERLOGGED = BooleanProperty.of("waterlogged");
 
     private static final VoxelShape UP_SHAPE = VoxelShapes.cuboid(0.125, 0, 0.125, 0.875, 0.5, 0.875);
