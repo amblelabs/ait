6db0f0a9d4d4dc19fbcb6b62294451fe8830d2b8
alex <70163067+rapbattlegod32@users.noreply.github.com>
2026-06-11 10:01:09 +0100

    theo nitpick part 50

--- src/main/java/dev/amble/ait/client/renderers/entities/ControlEntityRenderer.java
+++ src/main/java/dev/amble/ait/client/renderers/entities/ControlEntityRenderer.java
@@ -92,7 +92,7 @@ public class ControlEntityRenderer extends EntityRenderer<ConsoleControlEntity> 
             OrderedText nameOrdered = label.asOrderedText();
 
             if (isPlayerLookingWithSonic) {
-                textRenderer.drawWithOutline(nameOrdered, h, 0.0f, 0xF0F0F0, 0x000000,
+                textRenderer.drawWithOutline(nameOrdered, h, 0f, 0xF0F0F0, 0x000000,
                         matrix4f, vertexConsumers, 0xFF);
             }
         }
