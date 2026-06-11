9fc13bbd34f2ecbc844a8b4ce45a3ef4629fab19
Addi3 <addiec787@gmail.com>
2026-06-11 19:04:35 +0100

    chore: replaced RedstoneControlBlock sound

--- src/main/java/dev/amble/ait/core/AITSounds.java
+++ src/main/java/dev/amble/ait/core/AITSounds.java
@@ -210,7 +210,7 @@ public class AITSounds {
     public static final SoundEvent DING = register("tools/goes_ding");
     public static final SoundEvent STASER = register("tools/staser");
     public static final SoundEvent REMOTE = register("tools/remote");
-    public static final SoundEvent REDSTONE_CONTROL_SWITCHAROO = register("tools/redstone_control_switcharoo");
+   // public static final SoundEvent REDSTONE_CONTROL_SWITCHAROO = register("tools/redstone_control_switcharoo");
     public static final SoundEvent HAMMER_HIT = register("tools/hammer_hit");
     public static final SoundEvent COFFEE_MACHINE = register("tardis/coffee_machine");
 
