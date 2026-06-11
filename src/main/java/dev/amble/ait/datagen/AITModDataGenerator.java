de4b21a51e549a08430b21b817672e33ffc2d47c
bastalation <thesuicideman@outlook.com>
2026-06-11 20:19:40 +0100

    CHANGE

--- src/main/java/dev/amble/ait/datagen/AITModDataGenerator.java
+++ src/main/java/dev/amble/ait/datagen/AITModDataGenerator.java
@@ -1544,8 +1544,8 @@ public class AITModDataGenerator implements DataGeneratorEntrypoint {
         provider.addTranslation("waypoint.position.tooltip", "Position");
         provider.addTranslation("waypoint.dimension.tooltip", "Dimension");
         provider.addTranslation("waypoint.direction.tooltip", "Direction");
-        provider.addTranslation("ait.tooltip.coral_bad_rift", "Must be placed in Rift Chunk");
-        provider.addTranslation("ait.tooltip.coral_bad_block", "Must be placed on Soul Sand Block");
+        provider.addTranslation("coral_riftchunk", "Must be placed in Rift Chunk");
+        provider.addTranslation("coral_soulsand", "Must be placed on Soul Sand Block");
 
         // Blueprint Item
         provider.addTranslation("ait.blueprint.tooltip", "Blueprint: ");
