3fe72aaebf5815c0af1bea24e43cf3121cf8458c
Theo <git@theo.is-a.dev>
2026-06-16 14:55:59 +0300

    Implement clipboard copy for auth URL in TitleScreen
    Add clipboard functionality to copy auth URL when server is running.

--- src/main/java/dev/amble/ait/mixin/client/TitleScreenMixin.java
+++ src/main/java/dev/amble/ait/mixin/client/TitleScreenMixin.java
@@ -18,6 +18,10 @@ import net.minecraft.text.Text;
 import dev.amble.ait.AITMod;
 import dev.amble.ait.client.AITModClient;
 
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
 @Mixin(value = TitleScreen.class, priority = 999)
 public abstract class TitleScreenMixin extends Screen {
 
@@ -45,6 +49,13 @@ public abstract class TitleScreenMixin extends Screen {
         boolean beta = AITMod.isBetaLocked();
 
         ButtonWidget.Builder instance = new ButtonWidget.Builder(beta ? Text.translatable("text.ait.beta.play") : message, button -> {
+            if (BetaVerification.isServerRunning()) {
+                // TODO: maybe use whatever minecraft is using? if it isn't using this ig?
+                StringSelection stringSelection = new StringSelection(BetaVerification.getAuthUrl());
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(stringSelection, null);
+                return;
+            }
             if (AITMod.isBetaLocked() && !BetaTokenPrefs.isTokenValid()) {
                 button.setMessage(Text.translatable("text.ait.beta.play.browser"));
                 BetaVerification.startAndWaitForToken(valid -> {
