49537c94f0b74fafd9fb3b986113d5e14b610b21
Theo <git@theo.is-a.dev>
2026-06-30 17:33:19 +0300

    Add mallet check before removing durability

--- src/main/java/dev/amble/ait/core/entities/ConsoleControlEntity.java
+++ src/main/java/dev/amble/ait/core/entities/ConsoleControlEntity.java
@@ -475,7 +475,7 @@ public class ConsoleControlEntity extends LinkableDummyEntity {
         }
 
         if (state == DurabilityStates.JAMMED) {
-            if (random.nextBetween(0, 5) < 2)
+            if (hasMallet && random.nextBetween(0, 5) < 2)
                 tardis.subsystems().engine().removeDurability(random.nextBetween(0, 7));
 
             return false;
