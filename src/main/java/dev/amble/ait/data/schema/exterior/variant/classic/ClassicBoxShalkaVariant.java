63ae10e9ccb69e9d2c0e512766e25030ecf896d6
Loqor <isaiahcrace@gmail.com>
2026-07-03 14:18:16 -0500

    fix: shalka is back
    Signed-off-by: Loqor <isaiahcrace@gmail.com>

--- src/main/java/dev/amble/ait/data/schema/exterior/variant/classic/ClassicBoxShalkaVariant.java
+++ src/main/java/dev/amble/ait/data/schema/exterior/variant/classic/ClassicBoxShalkaVariant.java
@@ -1,12 +1,20 @@
 package dev.amble.ait.data.schema.exterior.variant.classic;
 
+import java.util.Optional;
+
+import dev.amble.ait.data.Loyalty;
 import dev.amble.ait.data.schema.door.DoorSchema;
 import dev.amble.ait.data.schema.door.impl.ClassicHudolinDoorVariant;
 import dev.amble.ait.registry.impl.door.DoorRegistry;
 
-public class ClassicBoxShalkaVariant extends ClassicBoxHudolinVariant {
+public class ClassicBoxShalkaVariant extends ClassicBoxVariant {
     public ClassicBoxShalkaVariant() {
-        super();
+        super("shalka");
+    }
+
+    @Override
+    public Optional<Loyalty> requirement() {
+        return Optional.of(new Loyalty(Loyalty.Type.COMPANION));
     }
 
     @Override
