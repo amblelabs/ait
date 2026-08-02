52d973dde7a068cdc8a918ae29d854560a0c737d
alex <70163067+rapbattlegod32@users.noreply.github.com>
2026-08-02 02:06:01 +0100

    Replace "shift to dismount" with "Hold shift for 3 seconds to dismount"

--- src/main/java/dev/amble/ait/datagen/AITModDataGenerator.java
+++ src/main/java/dev/amble/ait/datagen/AITModDataGenerator.java
@@ -1611,6 +1611,7 @@ public class AITModDataGenerator implements DataGeneratorEntrypoint {
         provider.addTranslation("overlay.ait.rwf.doors", "%s - Doors");
         provider.addTranslation("overlay.ait.rwf.ascend", "%s - Ascend");
         provider.addTranslation("overlay.ait.rwf.land", "%s - Descend / Land");
+        provider.addTranslation("overlay.ait.rwf.dismount", "Hold %s for 3 seconds to dismount");
 
         // effects
         provider.addTranslation("effect.ait.zeiton_high", "Zeiton High");
