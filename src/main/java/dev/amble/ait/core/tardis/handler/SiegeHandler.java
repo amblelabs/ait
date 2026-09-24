69946f7e8c59a44f89405c82b15d50ea3d57da2d
CVE-2017-0144 <twingod3@gmail.com>
2026-09-24 22:22:52 +0300

    fix: clear siege cubes from the whole inventory on logout

--- src/main/java/dev/amble/ait/core/tardis/handler/SiegeHandler.java
+++ src/main/java/dev/amble/ait/core/tardis/handler/SiegeHandler.java
@@ -20,8 +20,6 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
@@ -67,13 +65,10 @@ public class SiegeHandler extends KeyedTardisComponent implements TardisTickable
                 if (!Objects.equals(tardis.siege().getHeldPlayerUUID(), player.getUuid()))
                     return;
 
-                for (ItemStack itemStack : player.getInventory().main) {
-                    if (itemStack.isOf(AITItems.SIEGE_ITEM)) {
-                        if (tardis.getUuid().equals(SiegeTardisItem.getTardisIdStatic(itemStack))) {
-                            player.getInventory().setStack(player.getInventory().getSlotWithStack(itemStack), Items.AIR.getDefaultStack());
-                        }
-                    }
-                }
+                player.getInventory().remove(
+                        stack -> stack.isOf(AITItems.SIEGE_ITEM) && tardis.getUuid().equals(SiegeTardisItem.getTardisIdStatic(stack)),
+                        -1, player.playerScreenHandler.getCraftingInput());
+
                 SiegeTardisItem.placeTardis(tardis, SiegeTardisItem.fromEntity(player));
             });
         });
