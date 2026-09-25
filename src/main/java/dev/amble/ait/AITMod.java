1dac8acee3e73787b5a2f194b9a82eea10b0afca
CVE-2017-0144 <twingod3@gmail.com>
2026-09-25 21:07:38 +0300

    fix: use the vanilla reach check for block pos packets

--- src/main/java/dev/amble/ait/AITMod.java
+++ src/main/java/dev/amble/ait/AITMod.java
@@ -86,6 +86,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
@@ -276,7 +277,7 @@ public class AITMod implements ModInitializer {
                     String landingCode = buf.readString();
 
                     server.execute(() -> {
-                        if (!player.getWorld().isChunkLoaded(pos))
+                        if (player.getEyePos().squaredDistanceTo(pos.toCenterPos()) > ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE)
                             return;
 
                         LandingPadRegion region = LandingPadManager.getInstance((ServerWorld) player.getWorld()).getRegionAt(pos);
@@ -331,7 +332,7 @@ public class AITMod implements ModInitializer {
             server.execute(() -> {
                 World world = player.getWorld();
 
-                if (!world.isChunkLoaded(pos))
+                if (player.getEyePos().squaredDistanceTo(pos.toCenterPos()) > ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE)
                     return;
 
                 BlockState state = world.getBlockState(pos);
@@ -350,7 +351,8 @@ public class AITMod implements ModInitializer {
             Identifier id = buf.readIdentifier();
             server.execute(() -> {
                 ServerWorld world = player.getServerWorld();
-                if (world.isChunkLoaded(pos) && world.getBlockEntity(pos) instanceof EnvironmentProjectorBlockEntity projector) {
+                if (player.getEyePos().squaredDistanceTo(pos.toCenterPos()) <= ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE
+                        && world.getBlockEntity(pos) instanceof EnvironmentProjectorBlockEntity projector) {
                     RegistryKey<World> key = RegistryKey.of(RegistryKeys.WORLD, id);
 
                     if (!WorldUtil.getProjectorWorlds().contains(server.getWorld(key)))
@@ -367,7 +369,8 @@ public class AITMod implements ModInitializer {
             float pitch = buf.readFloat();
             server.execute(() -> {
                 ServerWorld world = player.getServerWorld();
-                if (world.isChunkLoaded(pos) && world.getBlockEntity(pos) instanceof EnvironmentProjectorBlockEntity projector) {
+                if (player.getEyePos().squaredDistanceTo(pos.toCenterPos()) <= ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE
+                        && world.getBlockEntity(pos) instanceof EnvironmentProjectorBlockEntity projector) {
                     projector.setAnglesFromClient(yaw, pitch, player);
                 }
             });
